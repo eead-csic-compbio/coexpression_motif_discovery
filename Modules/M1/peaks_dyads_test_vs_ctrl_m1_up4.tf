e14006d5f6f2a718b989da2daa409bdba3fa625e
najlaksouri <57443324+najlaksouri@users.noreply.github.com>
2019-11-20 15:49:59 +0100

    Add files via upload

---
+++ Modules/M1/peaks_dyads_test_vs_ctrl_m1_up4.tf
@@ -0,0 +1,90 @@
+AC  dyads_test_vs_ctrl_m1
+XX
+ID  dyads_test_vs_ctrl_m1
+XX
+DE  khyCTCTywtcCTCTCtCwCTCtm
+P0       a     c     g     t
+1        7     8     9    11
+2        9    12     5     9
+3        8    16     0    11
+4        3    27     2     3
+5        0     5     1    29
+6        3    29     2     1
+7        2     3     2    28
+8        6     9     3    17
+9       17     6     1    11
+10       3     6     2    24
+11       4    21     2     8
+12       3    30     1     1
+13       6     2     0    27
+14       0    34     0     1
+15       1     2     0    32
+16       1    28     3     3
+17       4     6     1    24
+18       2    30     2     1
+19      19     4     2    10
+20       1    29     1     4
+21       1     5     1    28
+22       2    29     2     2
+23       8     5     5    17
+24      11    15     1     8
+XX
+BA  35 sequences
+XX
+BS  GCTCTCTTTCTCACTCTCACTCTT; site_0; 1; 24; 0; p
+BS  CCTCTATCGTCCTCTGTCTCTCGT; site_1; 1; 24; 0; p
+BS  ATTCTCTCCCCCTCTCTCACTGGC; site_2; 1; 24; 0; p
+BS  AAACTCCCACCCCCTCTCACTCTC; site_3; 1; 24; 0; p
+BS  TCTCTGTTATTCTCTCCCTGTCAC; site_4; 1; 24; 0; p
+BS  GACGCCGTCTCCACTCTCACTCTC; site_5; 1; 24; 0; p
+BS  GACGCCGTCTCCACTCTCACTCTC; site_6; 1; 24; 0; p
+BS  TACTTCTCTTTCTCTCTCACTTCA; site_7; 1; 24; 0; p
+BS  GTCCTACTTCCCTCTCTCACTCCC; site_8; 1; 24; 0; p
+BS  CGTCTCTGAAACTCTCTCACACTC; site_9; 1; 24; 0; p
+BS  ACTCTCTCTACCTCTCTGACTCTT; site_10; 1; 24; 0; p
+BS  TCCTTCTTTTCCTCTTTCTATCAA; site_11; 1; 24; 0; p
+BS  CCACTCTCATCCACACACTCTCAA; site_12; 1; 24; 0; p
+BS  TCACTCACATCGTCTCTCCCCCTA; site_13; 1; 24; 0; p
+BS  ATCCCTTTATCCTCTCCCACCAGC; site_14; 1; 24; 0; p
+BS  TTCCTCTTATTCTCTTGCATTACA; site_15; 1; 24; 0; p
+BS  GGCATCTGATTCTCTCTCATCCGC; site_16; 1; 24; 0; p
+BS  GACCTCTATCTCTCTCACACTCTT; site_17; 1; 24; 0; p
+BS  CAACTCTAATACTCTCTCTTCCAA; site_18; 1; 24; 0; p
+BS  GACCTCTATCTCTCTCACACTCTT; site_19; 1; 24; 0; p
+BS  CAACTCTAATACTCTCTCTTCCAA; site_20; 1; 24; 0; p
+BS  TGCCTCTGAGCAACTCTAACTCTA; site_21; 1; 24; 0; p
+BS  GCTCTCTCATCATCTCTATCTTAT; site_22; 1; 24; 0; p
+BS  CTACTCCAATCCTCTGCCTCTCAC; site_23; 1; 24; 0; p
+BS  ATCCTGTTATCCTTCACCACTCAA; site_24; 1; 24; 0; p
+BS  TTCATCAACTCCTCTCCCACTCGA; site_25; 1; 24; 0; p
+BS  TCTCTCTTTTTCTCTTTCACTGTG; site_26; 1; 24; 0; p
+BS  AACCTCTCTGCCACTCTCTCTCCC; site_27; 1; 24; 0; p
+BS  CGTATATTCTACTCTCTCACTCTC; site_28; 1; 24; 0; p
+BS  GCTTGCTTTTCCTCTCTCCCTCTA; site_29; 1; 24; 0; p
+BS  TCCCCCTTCTCATCTCTTCCTCTT; site_30; 1; 24; 0; p
+BS  ATACCCTTATCCCCTGTCGCGCCC; site_31; 1; 24; 0; p
+BS  CCCCTCTTTTGCTCTCTCGCTCTC; site_32; 1; 24; 0; p
+BS  TTTCTCTTAAGCTCTCACTCTCTC; site_33; 1; 24; 0; p
+BS  TGACTCTTATCTTCCCCGCCTCTT; site_34; 1; 24; 0; p
+CC  program: transfac
+CC  matrix.nb: 6
+CC  accession: dyads_test_vs_ctrl_m1
+CC  AC: dyads_test_vs_ctrl_m1
+CC  id: dyads_test_vs_ctrl_m1
+CC  name: dyads_test_vs_ctrl_m1
+CC  version: 
+CC  name: dyads_test_vs_ctrl_m1
+CC  description: khyCTCTywtcCTCTCtCwCTCtm
+CC  statistical_basis: 35 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 6
+CC  sites: 35
+CC  bg_markov_order: 1
+CC  consensus.strict: gccCTCTtatcCTCTCtCaCTCtc
+CC  consensus.strict.rc: GAGAGTGAGAGAGGATAAGAGGGC
+CC  consensus.IUPAC: kcyCTCTywtcCTCTCtCwCTCtm
+CC  consensus.IUPAC.rc: KAGAGWGAGAGAGGAWRAGAGRGM
+CC  consensus.regexp: [gt]c[ct]CTCT[ct][at]tcCTCTCtC[at]CTCt[ac]
+CC  consensus.regexp.rc: [GT]AGAG[AT]GAGAGAGGA[AT][AG]AGAG[AG]G[AC]
+XX
+//
---
+++ Modules/M1/peaks_oligos_5-8nt_m2_up4.tf
@@ -0,0 +1,86 @@
+AC  oligos_5-8nt_m2
+XX
+ID  oligos_5-8nt_m2
+XX
+DE  hkCcTTwTCCaCTCTCTstsyy
+P0       a     c     g     t
+1        9    12     3     9
+2        8     6    10     9
+3        3    25     2     3
+4        4    21     1     7
+5        2     1     6    24
+6        3     4     0    26
+7       18     4     1    10
+8        2     4     1    26
+9        0    25     4     4
+10       1    31     1     0
+11      17     4     5     7
+12       1    30     1     1
+13       0     2     1    30
+14       0    29     1     3
+15       2     0     1    30
+16       1    29     3     0
+17       3     2     2    26
+18       1    10    16     6
+19       2     7     3    21
+20       4    12    10     7
+21       5    12     3    13
+22       7    15     0    11
+XX
+BA  33 sequences
+XX
+BS  ACGTTTATGCACTCTCTCTTTT; site_0; 1; 22; 0; p
+BS  TATATTTTCCACTCTCTGCATC; site_1; 1; 22; 0; p
+BS  ACCCTAATTCGCTCACTGAGTC; site_2; 1; 22; 0; p
+BS  AGATTTAACCTCTCTCTGTTCA; site_3; 1; 22; 0; p
+BS  AAACTCTCCCACTCTCTGCGAA; site_4; 1; 22; 0; p
+BS  ATCTGTACCCTCTCTCTGTTCC; site_5; 1; 22; 0; p
+BS  CGCCGTCTCCACTCTCACTCTC; site_6; 1; 22; 0; p
+BS  CGCCGTCTCCACTCTCACTCTC; site_7; 1; 22; 0; p
+BS  CGCCTAAACCCCTCTGTGTAGA; site_8; 1; 22; 0; p
+BS  TTCCTTTTCCCCCCTCTCTCTA; site_9; 1; 22; 0; p
+BS  CACCTTTTTCTCTCTCTTTGCA; site_10; 1; 22; 0; p
+BS  TACCGTATCCGCCCTCTTTTCT; site_11; 1; 22; 0; p
+BS  CCCATTACGCGGTCTCTGTGAT; site_12; 1; 22; 0; p
+BS  TCCCTTTGCCTCTCTCTCTGCC; site_13; 1; 22; 0; p
+BS  GTCCGTTTGCACTCTCTCAGTC; site_14; 1; 22; 0; p
+BS  GATTTTGCCCACTCTCTGTCCT; site_15; 1; 22; 0; p
+BS  ATCCTTTTCCACTCAGTTTGTT; site_16; 1; 22; 0; p
+BS  CGCCTTTTCCACTCTGCTTCAC; site_17; 1; 22; 0; p
+BS  CTCCACATTCACTCTCTGCCCC; site_18; 1; 22; 0; p
+BS  TGCCTCATCCTCTCTCTGCAAC; site_19; 1; 22; 0; p
+BS  TTCTTCATCATCTCTCTGTCTC; site_20; 1; 22; 0; p
+BS  AGCCTTATCCATTTTCACTGCT; site_21; 1; 22; 0; p
+BS  CACCTACTCCACTCTCTTCTGC; site_22; 1; 22; 0; p
+BS  CGCTGTATCCGCTCGCTGCAAA; site_23; 1; 22; 0; p
+BS  GTCATTATGCACGCTCCGCCCT; site_24; 1; 22; 0; p
+BS  CACTTTATCCTCTCTCGTGGTC; site_25; 1; 22; 0; p
+BS  TCCATTATCCACTCTCTCTTCC; site_26; 1; 22; 0; p
+BS  ACCCTTATCCCCTGTCGCGCCC; site_27; 1; 22; 0; p
+BS  TGACTTTTTGACTCTCTGTTTT; site_28; 1; 22; 0; p
+BS  ATCCATCTCCACTCTCTCGCTT; site_29; 1; 22; 0; p
+BS  CGCGTTTTCCCATCTCTGTCCT; site_30; 1; 22; 0; p
+BS  TTGCTTATCCGCTTTCTGTCGT; site_31; 1; 22; 0; p
+BS  CATCCTATCCACTTTATATGTA; site_32; 1; 22; 0; p
+CC  program: transfac
+CC  matrix.nb: 2
+CC  accession: oligos_5-8nt_m2
+CC  AC: oligos_5-8nt_m2
+CC  id: oligos_5-8nt_m2
+CC  name: oligos_5-8nt_m2
+CC  version: 
+CC  name: oligos_5-8nt_m2
+CC  description: hkCcTTwTCCaCTCTCTstsyy
+CC  statistical_basis: 33 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 2
+CC  sites: 33
+CC  bg_markov_order: 1
+CC  consensus.strict: cgCctTaTCCaCTCTCTgtgcc
+CC  consensus.strict.rc: GGCACAGAGAGTGGATAAGGCG
+CC  consensus.IUPAC: cgCctTwTCCaCTCTCTstsyy
+CC  consensus.IUPAC.rc: RRSASAGAGAGTGGAWAAGGCG
+CC  consensus.regexp: cgCctT[at]TCCaCTCTCT[cg]t[cg][ct][ct]
+CC  consensus.regexp.rc: [AG][AG][CG]A[CG]AGAGAGTGGA[AT]AAGGCG
+XX
+//
