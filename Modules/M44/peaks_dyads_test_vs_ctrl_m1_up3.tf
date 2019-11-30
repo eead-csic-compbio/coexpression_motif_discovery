df3238666051fe3cb5c4faf177b08867fd1c9471
najlaksouri <57443324+najlaksouri@users.noreply.github.com>
2019-11-30 16:11:52 +0100

    Add files via upload

---
+++ Modules/M44/peaks_dyads_test_vs_ctrl_m1_up3.tf
@@ -0,0 +1,74 @@
+AC  dyads_test_vs_ctrl_m1
+XX
+ID  dyads_test_vs_ctrl_m1
+XX
+DE  wwCTCGCAwr
+P0       a     c     g     t
+1       11     7     6     9
+2       11     6     4    12
+3        0    32     1     0
+4        4     0     4    25
+5        0    33     0     0
+6        0     0    32     1
+7        1    25     1     6
+8       32     0     1     0
+9        9     5     4    15
+10       9     2    15     7
+XX
+BA  33 sequences
+XX
+BS  AACTCGCATA; site_0; 1; 10; 0; p
+BS  CCCTCGCATA; site_1; 1; 10; 0; p
+BS  CTCTCGTAAA; site_2; 1; 10; 0; p
+BS  ACCTCGCAGT; site_3; 1; 10; 0; p
+BS  TACACGCAAT; site_4; 1; 10; 0; p
+BS  CTCTCGCATG; site_5; 1; 10; 0; p
+BS  TCCGCGCATG; site_6; 1; 10; 0; p
+BS  ATCTCGTACG; site_7; 1; 10; 0; p
+BS  TTCTCGCAAG; site_8; 1; 10; 0; p
+BS  CTCTCGCATG; site_9; 1; 10; 0; p
+BS  ACCTCGCAGG; site_10; 1; 10; 0; p
+BS  GACTCGTATA; site_11; 1; 10; 0; p
+BS  GACTCGCACG; site_12; 1; 10; 0; p
+BS  CACTCGTAAT; site_13; 1; 10; 0; p
+BS  ACCACGCATG; site_14; 1; 10; 0; p
+BS  TAGTCGCAGT; site_15; 1; 10; 0; p
+BS  AACTCGCGCA; site_16; 1; 10; 0; p
+BS  CTCGCGCAAT; site_17; 1; 10; 0; p
+BS  TTCTCGAAGC; site_18; 1; 10; 0; p
+BS  CTCTCGGATA; site_19; 1; 10; 0; p
+BS  GTCTCTCATA; site_20; 1; 10; 0; p
+BS  TACTCGCATC; site_21; 1; 10; 0; p
+BS  GTCTCGTATT; site_22; 1; 10; 0; p
+BS  AGCACGCATG; site_23; 1; 10; 0; p
+BS  TTCGCGCAAG; site_24; 1; 10; 0; p
+BS  AACTCGCACA; site_25; 1; 10; 0; p
+BS  AGCACGCATG; site_26; 1; 10; 0; p
+BS  TTCGCGCAAG; site_27; 1; 10; 0; p
+BS  AACTCGCACA; site_28; 1; 10; 0; p
+BS  GCCTCGCATG; site_29; 1; 10; 0; p
+BS  TACTCGTATG; site_30; 1; 10; 0; p
+BS  GGCTCGCAAT; site_31; 1; 10; 0; p
+BS  AGCTCGCAAG; site_32; 1; 10; 0; p
+CC  program: transfac
+CC  matrix.nb: 3
+CC  accession: dyads_test_vs_ctrl_m1
+CC  AC: dyads_test_vs_ctrl_m1
+CC  id: dyads_test_vs_ctrl_m1
+CC  name: dyads_test_vs_ctrl_m1
+CC  version: 
+CC  name: dyads_test_vs_ctrl_m1
+CC  description: wwCTCGCAwr
+CC  statistical_basis: 33 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 3
+CC  sites: 33
+CC  bg_markov_order: 1
+CC  consensus.strict: ctCtCGCAtg
+CC  consensus.strict.rc: CATGCGAGAG
+CC  consensus.IUPAC: mhCtCGCAtg
+CC  consensus.IUPAC.rc: CATGCGAGDK
+CC  consensus.regexp: [ac][act]CtCGCAtg
+CC  consensus.regexp.rc: CATGCGAG[AGT][GT]
+XX
+//
---
+++ Modules/M44/peaks_oligos_5-8nt_m1_up3.tf
@@ -0,0 +1,76 @@
+AC  oligos_5-8nt_m1
+XX
+ID  oligos_5-8nt_m1
+XX
+DE  wwCTCGCAwrw
+P0       a     c     g     t
+1       11     7     6    10
+2       11     8     4    11
+3        0    33     1     0
+4        4     0     4    26
+5        0    34     0     0
+6        0     0    33     1
+7        0    25     3     6
+8       33     0     1     0
+9        9     5     3    17
+10       9     1    17     7
+11       9     7     6    12
+XX
+BA  34 sequences
+XX
+BS  AACTCGCATAT; site_0; 1; 11; 0; p
+BS  CCCTCGCATAA; site_1; 1; 11; 0; p
+BS  CTCTCGTAAAC; site_2; 1; 11; 0; p
+BS  ACCTCGCAGTT; site_3; 1; 11; 0; p
+BS  TACACGCAATT; site_4; 1; 11; 0; p
+BS  CTCTCGCATGC; site_5; 1; 11; 0; p
+BS  TCCGCGCATGC; site_6; 1; 11; 0; p
+BS  ATCTCGTACGT; site_7; 1; 11; 0; p
+BS  TTCTCGCAAGA; site_8; 1; 11; 0; p
+BS  CTCTCGCATGC; site_9; 1; 11; 0; p
+BS  ACCTCGCAGGG; site_10; 1; 11; 0; p
+BS  GACTCGTATAC; site_11; 1; 11; 0; p
+BS  GACTCGCACGT; site_12; 1; 11; 0; p
+BS  CACTCGTAATT; site_13; 1; 11; 0; p
+BS  ACCACGCATGG; site_14; 1; 11; 0; p
+BS  TAGTCGCAGTA; site_15; 1; 11; 0; p
+BS  AACTCGCGCAA; site_16; 1; 11; 0; p
+BS  CTCGCGCAATG; site_17; 1; 11; 0; p
+BS  CTCTCGGATAA; site_18; 1; 11; 0; p
+BS  GTCTCTCATAA; site_19; 1; 11; 0; p
+BS  TACTCGCATCT; site_20; 1; 11; 0; p
+BS  GTCTCGTATTT; site_21; 1; 11; 0; p
+BS  AGCACGCATGC; site_22; 1; 11; 0; p
+BS  TCCTCGGATGT; site_23; 1; 11; 0; p
+BS  TTCGCGCAAGA; site_24; 1; 11; 0; p
+BS  AACTCGCACAT; site_25; 1; 11; 0; p
+BS  AGCACGCATGC; site_26; 1; 11; 0; p
+BS  TCCTCGGATGT; site_27; 1; 11; 0; p
+BS  TTCGCGCAAGA; site_28; 1; 11; 0; p
+BS  AACTCGCACAT; site_29; 1; 11; 0; p
+BS  GCCTCGCATGG; site_30; 1; 11; 0; p
+BS  TACTCGTATGG; site_31; 1; 11; 0; p
+BS  GGCTCGCAATA; site_32; 1; 11; 0; p
+BS  AGCTCGCAAGG; site_33; 1; 11; 0; p
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: oligos_5-8nt_m1
+CC  AC: oligos_5-8nt_m1
+CC  id: oligos_5-8nt_m1
+CC  name: oligos_5-8nt_m1
+CC  version: 
+CC  name: oligos_5-8nt_m1
+CC  description: wwCTCGCAwrw
+CC  statistical_basis: 34 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  sites: 34
+CC  bg_markov_order: 1
+CC  consensus.strict: ccCtCGCAtgc
+CC  consensus.strict.rc: GCATGCGAGGG
+CC  consensus.IUPAC: mhCtCGCAtgy
+CC  consensus.IUPAC.rc: RCATGCGAGDK
+CC  consensus.regexp: [ac][act]CtCGCAtg[ct]
+CC  consensus.regexp.rc: [AG]CATGCGAG[AGT][GT]
+XX
+//
