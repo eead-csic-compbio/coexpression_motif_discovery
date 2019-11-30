fbfc993b4093cb5e14a2df53988334b06313227a
najlaksouri <57443324+najlaksouri@users.noreply.github.com>
2019-11-30 15:17:17 +0100

    Add files via upload

---
+++ Modules/M25/peaks_oligos_5-8nt_m1_up4.tf
@@ -0,0 +1,61 @@
+AC  oligos_5-8nt_m1
+XX
+ID  oligos_5-8nt_m1
+XX
+DE  WyArwTATMGTGAGTTkb
+P0       a     c     g     t
+1        3     0     0     9
+2        2     5     0     5
+3        9     1     1     1
+4        6     1     4     1
+5        5     1     0     6
+6        1     1     0    10
+7       11     0     1     0
+8        0     0     0    12
+9        3     9     0     0
+10       0     1    11     0
+11       1     0     0    11
+12       1     0    11     0
+13      12     0     0     0
+14       0     0    10     2
+15       0     1     0    11
+16       2     1     0     9
+17       2     0     4     6
+18       0     3     6     3
+XX
+BA  12 sequences
+XX
+BS  ATAGATATACTGAGTTTG; site_0; 1; 18; 0; p
+BS  TATCCAATCGTGATTTGT; site_1; 1; 18; 0; p
+BS  TTGGTCGTCGTGAGCTAC; site_2; 1; 18; 0; p
+BS  TAATATATCGAAAGTTGT; site_3; 1; 18; 0; p
+BS  TCAATTATCGTGAGTTTG; site_4; 1; 18; 0; p
+BS  TCAATTATCGTGAGTTTG; site_5; 1; 18; 0; p
+BS  TCAATTATCGTGAGTTTG; site_6; 1; 18; 0; p
+BS  TCAATTATCGTGAGTTTG; site_7; 1; 18; 0; p
+BS  TCAATTATCGTGAGTTTG; site_8; 1; 18; 0; p
+BS  TTCAATATCGTGATTCAT; site_9; 1; 18; 0; p
+BS  ATAGATATAGTGAGTAGC; site_10; 1; 18; 0; p
+BS  ATAGATATAGTGAGTAGC; site_11; 1; 18; 0; p
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: oligos_5-8nt_m1
+CC  AC: oligos_5-8nt_m1
+CC  id: oligos_5-8nt_m1
+CC  name: oligos_5-8nt_m1
+CC  version: 
+CC  name: oligos_5-8nt_m1
+CC  description: WyArwTATMGTGAGTTkb
+CC  statistical_basis: 12 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  sites: 12
+CC  bg_markov_order: 1
+CC  consensus.strict: tcagtTATCGTGAGTtgg
+CC  consensus.strict.rc: CCAACTCACGATAACTGA
+CC  consensus.IUPAC: tyarwTATCGTGAGTtks
+CC  consensus.IUPAC.rc: SMAACTCACGATAWYTRA
+CC  consensus.regexp: t[ct]a[ag][at]TATCGTGAGTt[gt][cg]
+CC  consensus.regexp.rc: [CG][AC]AACTCACGATA[AT][CT]T[AG]A
+XX
+//
