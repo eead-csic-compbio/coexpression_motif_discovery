17951fb88b4cb982051b3ea84618cb6c5420910a
najlaksouri <57443324+najlaksouri@users.noreply.github.com>
2020-09-21 13:21:17 +0200

    Add files via upload

---
+++ peach/Modules/M24/peaks_dyads_test_vs_ctrl_m1_up4.tf
@@ -0,0 +1,111 @@
+AC  dyads_test_vs_ctrl_m1
+XX
+ID  dyads_test_vs_ctrl_m1
+XX
+DE  hyaAAACCCTAGyw
+P0       a     c     g     t
+1       17    17    14    18
+2       10    22    15    19
+3       35    13    11     7
+4       63     1     0     2
+5       66     0     0     0
+6       64     0     2     0
+7       14    50     1     1
+8        2    60     2     2
+9        0    65     0     1
+10       1     2     0    63
+11      52     4     5     5
+12      11     7    46     2
+13      16    23    10    17
+14      18    14    14    20
+XX
+BA  66 sequences
+XX
+BS  ACTAAAAGCTAGGG; site_0; 1; 14; 0; p
+BS  ACCAAACCTTAGTG; site_1; 1; 14; 0; p
+BS  TGTAAAAGCTAGGA; site_2; 1; 14; 0; p
+BS  CCAAAACCCAAGGG; site_3; 1; 14; 0; p
+BS  TGTAAAATCTAGCA; site_4; 1; 14; 0; p
+BS  GGAAAACCCTAGTT; site_5; 1; 14; 0; p
+BS  CGAAAACCCTGCCA; site_6; 1; 14; 0; p
+BS  TTCAAGCCCTAGAA; site_7; 1; 14; 0; p
+BS  GAGAAACCCTAACT; site_8; 1; 14; 0; p
+BS  TCAAAACCCTCTGC; site_9; 1; 14; 0; p
+BS  GCAAAACCCTCACT; site_10; 1; 14; 0; p
+BS  GCAAAACCCTAGCT; site_11; 1; 14; 0; p
+BS  TGAAAACCCTCGCG; site_12; 1; 14; 0; p
+BS  TGCAAACCCTAGCT; site_13; 1; 14; 0; p
+BS  ACAAAACCCTAGAC; site_14; 1; 14; 0; p
+BS  ACAAAACCCTAGAC; site_15; 1; 14; 0; p
+BS  GTAAAACCCTACCA; site_16; 1; 14; 0; p
+BS  CGCTAACCCTAGTT; site_17; 1; 14; 0; p
+BS  ACAAAACCCTACTA; site_18; 1; 14; 0; p
+BS  TCCAAACCCTAGCC; site_19; 1; 14; 0; p
+BS  TTGAAACCCTAGGT; site_20; 1; 14; 0; p
+BS  CTAAAACCCTAAAT; site_21; 1; 14; 0; p
+BS  AACAAAGCCTAGAG; site_22; 1; 14; 0; p
+BS  CTAAAACCCTAAAT; site_23; 1; 14; 0; p
+BS  TCGAAACCCTAGTG; site_24; 1; 14; 0; p
+BS  TCGAAACCCTAGTG; site_25; 1; 14; 0; p
+BS  CGGAAACCCTAGCG; site_26; 1; 14; 0; p
+BS  CTCAAAACCTTGCT; site_27; 1; 14; 0; p
+BS  CTTAAACCCCAGAT; site_28; 1; 14; 0; p
+BS  TCAAAACCCTAGCT; site_29; 1; 14; 0; p
+BS  CTAAAACCCTAGAA; site_30; 1; 14; 0; p
+BS  ACCAAAACCTTGTG; site_31; 1; 14; 0; p
+BS  ACCAAAACCTTGTG; site_32; 1; 14; 0; p
+BS  TCAAAACCCTAATC; site_33; 1; 14; 0; p
+BS  GGGAAAACCTAGCC; site_34; 1; 14; 0; p
+BS  TGAAAAACCTAGGC; site_35; 1; 14; 0; p
+BS  ATGAAAACCTCGTG; site_36; 1; 14; 0; p
+BS  GTACAACTCTAGAT; site_37; 1; 14; 0; p
+BS  CTAAAACCCTGCTC; site_38; 1; 14; 0; p
+BS  GAAAAACCCTAGAT; site_39; 1; 14; 0; p
+BS  AAAAAACCCTACTC; site_40; 1; 14; 0; p
+BS  CTAAAACCCTTGGC; site_41; 1; 14; 0; p
+BS  GGAAAATCCTAGCA; site_42; 1; 14; 0; p
+BS  TAAAAACACTGGTT; site_43; 1; 14; 0; p
+BS  ACAAAACCCCGGTT; site_44; 1; 14; 0; p
+BS  ACAAAACCCTAGTA; site_45; 1; 14; 0; p
+BS  GCCAAAACCTAAGG; site_46; 1; 14; 0; p
+BS  CTAAAACCCTAGCA; site_47; 1; 14; 0; p
+BS  ATAAAACCCTTGCA; site_48; 1; 14; 0; p
+BS  TGAAAACCCTAGAA; site_49; 1; 14; 0; p
+BS  TTGAAACCCTAAAC; site_50; 1; 14; 0; p
+BS  TAAAAAACCTGATA; site_51; 1; 14; 0; p
+BS  GTAAAGCCCTATCC; site_52; 1; 14; 0; p
+BS  ACCTAAACCTAGGG; site_53; 1; 14; 0; p
+BS  GCGAAACCCTAGGT; site_54; 1; 14; 0; p
+BS  GAAAAACCCTAAAT; site_55; 1; 14; 0; p
+BS  TTTAAACCCTAGAA; site_56; 1; 14; 0; p
+BS  CGTAAACCCTAGAA; site_57; 1; 14; 0; p
+BS  AGAAAACACTAGAA; site_58; 1; 14; 0; p
+BS  GCAAAACCCTAATA; site_59; 1; 14; 0; p
+BS  CTCAAAACCTACCA; site_60; 1; 14; 0; p
+BS  CATAAACCCTACCT; site_61; 1; 14; 0; p
+BS  CGAAAACCCTAGCG; site_62; 1; 14; 0; p
+BS  AAGAAACCCTAGCC; site_63; 1; 14; 0; p
+BS  AAGAAACCCTAGCC; site_64; 1; 14; 0; p
+BS  CTCAAAACCTAACT; site_65; 1; 14; 0; p
+CC  program: transfac
+CC  matrix.nb: 6
+CC  accession: dyads_test_vs_ctrl_m1
+CC  AC: dyads_test_vs_ctrl_m1
+CC  id: dyads_test_vs_ctrl_m1
+CC  name: dyads_test_vs_ctrl_m1
+CC  version: 
+CC  name: dyads_test_vs_ctrl_m1
+CC  description: hyaAAACCCTAGyw
+CC  statistical_basis: 66 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 6
+CC  sites: 66
+CC  bg_markov_order: 1
+CC  consensus.strict: gcaAAACCCTAGct
+CC  consensus.strict.rc: AGCTAGGGTTTTGC
+CC  consensus.IUPAC: bbaAAACCCTAGck
+CC  consensus.IUPAC.rc: MGCTAGGGTTTTVV
+CC  consensus.regexp: [cgt][cgt]aAAACCCTAGc[gt]
+CC  consensus.regexp.rc: [AC]GCTAGGGTTTT[ACG][ACG]
+XX
+//
